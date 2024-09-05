ab659984df256fdbc0520579eb2f6f3c7cd8841a
Sauradip Ghosh <sauradip96ghosh@gmail.com>
2024-09-05 10:07:04 +0530

    About section added
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css';
 import { Hero } from './components/Hero';
-import { Footer } from './components/Footer'
+import { About } from './components/About';
+import { Footer } from './components/Footer';
 function App() {
   
 
   return (
     <div className='text-azureBlue font-open mx-auto'>
       <Hero />
+      <About />
       <Footer />
     </div>
   )
