db59ee31e13696f6ad7ff7b520ba2832cc19a6b5
Sauradip Ghosh <sauradip96ghosh@gmail.com>
2024-09-05 09:58:40 +0530

    Footer section render
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css';
 import { Hero } from './components/Hero';
-
+import { Footer } from './components/Footer'
 function App() {
   
 
   return (
     <div className='text-azureBlue font-open mx-auto'>
       <Hero />
+      <Footer />
     </div>
   )
 }
